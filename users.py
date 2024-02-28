858bdc204e7653ec63de423a0068bc494288de66
Juan Sanchez <juanjosesanchezgil@gmail.com>
2024-02-28 21:22:38 +0100

    users

--- users.py
+++ users.py
@@ -4,4 +4,6 @@ app = FastAPI()
 
 @app.get("/users")
 async def users():
-    return "Hola Users!"
+    return [{"name" : "Brais", "surname" : "moure", "url" : "https://moure.dev" },
+            {"name" : "Moure", "surname" : "Dev", "url" : "https://mouredev.com" },
+            {"name" : "Juanj", "surname" : "Sanchez", "url" : "https://juansanchez.dev" }]
