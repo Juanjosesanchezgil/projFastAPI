1d3293883f38e90c5c397e34949737dd4217a14d
Juan Sanchez <juanjosesanchezgil@gmail.com>
2024-02-29 11:10:40 +0100

    users

--- users.py
+++ users.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Entidad User
+
+class User(BaseModel ):
+    name : str
+    surname : str
+    url : str
+    age : int
+
+users_list = [User(name="Brais", surname="Moure", url="https://moure.dev", age=35),
+         User(name="Moure", surname="Dev", url="https://mouredev.com", age=35),
+         User(name="Juan", surname="Sanchez", url="https://juansanchez.dev", age=39),
+         ]
+    
+
+@app.get("/usersjson")
+async def usersjson():
+    return [{"name" : "Brais", "surname" : "moure", "url" : "https://moure.dev", "age" : "35" },
+            {"name" : "Moure", "surname" : "Dev", "url" : "https://mouredev.com", "age" : "35" },
+            {"name" : "Juanj", "surname" : "Sanchez", "url" : "https://juansanchez.dev", "age" : "39" }]
+    
 @app.get("/users")
 async def users():
-    return [{"name" : "Brais", "surname" : "moure", "url" : "https://moure.dev" },
-            {"name" : "Moure", "surname" : "Dev", "url" : "https://mouredev.com" },
-            {"name" : "Juanj", "surname" : "Sanchez", "url" : "https://juansanchez.dev" }]
+    return users_list
